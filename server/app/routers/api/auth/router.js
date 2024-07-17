c043619e98ef30b0e49e83a0f8b3ed82cf5e74c8
Delgorithm <delgorithm@gmail.com>
2024-07-17 16:06:28 +0200

    Routes

--- server/app/routers/api/auth/router.js
+++ server/app/routers/api/auth/router.js
@@ -1,15 +1,17 @@
+// routers/api/auth/router.js
+
 const express = require("express");
 
 const router = express.Router();
 
-const userActions = require("../../../controllers/usersActions");
+const usersActions = require("../../../controllers/usersActions");
 const { verifyUser } = require("../../../middleware/verifyUser");
 const { verifyToken } = require("../../../middleware/verifyToken");
 const {
   verifyHashPassword,
 } = require("../../../middleware/verifyHashPassword");
 
-router.post("/users", verifyHashPassword, userActions.add);
+router.post("/users", verifyHashPassword, usersActions.add);
 
 router.post("/login", verifyUser);
 
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,19 +1,19 @@
+// routers/api/router.js
+
 const express = require("express");
 
 const router = express.Router();
-
-/* ************************************************************************* */
-// Import And Use Routers Here
-/* ************************************************************************* */
-
-const expensesRouter = require("./expenses/router");
-
-router.use("/expenses", expensesRouter);
 
 const authRouter = require("./auth/router");
 
 router.use("/auth", authRouter);
 
-/* ************************************************************************* */
+const usersRouter = require("./users/router");
+
+router.use("/users", usersRouter);
+
+const expensesRouter = require("./expenses/router");
+
+router.use("/expenses", expensesRouter);
 
 module.exports = router;
