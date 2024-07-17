cdfb3b61b271ae0d9ab22f898536edc594f47403
Delgorithm <delgorithm@gmail.com>
2024-07-17 14:40:14 +0200

    Pages : Login & Register

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import { Form, Link } from "react-router-dom";
+
+function Login() {
+  return (
+    <section>
+      <Form method="POST">
+        <h2>Connexion</h2>
+
+        <label htmlFor="email">
+          E-mail
+          <input
+            type="text"
+            id="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            required
+            autoComplete="email"
+          />
+        </label>
+
+        <label htmlFor="password">
+          Mot de passe
+          <input
+            type="password"
+            id="password"
+            name="password"
+            placeholder="********"
+            required
+            autoComplete="new-password"
+          />
+        </label>
+
+        <button type="submit">Se connecter</button>
+
+        <p>
+          Pas encore inscrit ?<Link to="/register">Créer un compte</Link>
+        </p>
+      </Form>
+
+      <article>
+        <img
+          src="../src/assets/images/mockup.png"
+          alt="Mockups de l'application"
+        />
+      </article>
+    </section>
+  );
+}
+
+export default Login;
