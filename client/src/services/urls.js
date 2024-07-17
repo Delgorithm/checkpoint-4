8138ee9801386ad145697ae3215624fdd397cf0c
Delgorithm <delgorithm@gmail.com>
2024-07-17 14:42:03 +0200

    Services

---
+++ client/src/services/urls.js
@@ -0,0 +1,4 @@
+export const baseRegisterUrl = "/api/auth/users";
+export const baseLoginUrl = "/api/auth/login";
+export const baseUserUrl = "/api/users/";
+export const baseExpensesUrl = "/api/expenses";
