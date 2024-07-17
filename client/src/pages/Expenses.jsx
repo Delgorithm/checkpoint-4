dccede6f80774d350c05207c0b2c762cea7579aa
Delgorithm <delgorithm@gmail.com>
2024-07-17 14:34:29 +0200

    Page : Expenses

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -1,5 +1,15 @@
+import TrackExpenses from "../components/TrackExpenses";
+
 function Expenses() {
-  return <p>Expenses</p>;
+  return (
+    <>
+      <p>Expenses</p>
+      <TrackExpenses
+        method="POST"
+        action="http://localhost:3310/api/expenses/add"
+      />
+    </>
+  );
 }
 
 export default Expenses;
