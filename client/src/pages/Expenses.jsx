537186ca400e2c7db696cabb01509a548aca18d1
Delgorithm <delgorithm@gmail.com>
2024-07-17 18:52:28 +0200

    Fix : Added the Form directly in it. Will change after

--- client/src/pages/Expenses.jsx
+++ client/src/pages/Expenses.jsx
@@ -1,13 +1,41 @@
-import TrackExpenses from "../components/TrackExpenses";
+import { Form } from "react-router-dom";
 
 function Expenses() {
   return (
     <>
       <p>Expenses</p>
-      <TrackExpenses
-        method="POST"
-        action="http://localhost:3310/api/expenses/add"
-      />
+      <Form method="POST">
+        <h2>Ma dépense</h2>
+
+        <label htmlFor="category">
+          Catégorie
+          <select id="category" name="category" required>
+            <option value="Food">Nourriture</option>
+            <option value="Health">Santé</option>
+            <option value="Transport">Transport</option>
+            <option value="Entertainment">Plaisir</option>
+            <option value="Other">Autres</option>
+          </select>
+        </label>
+
+        <label htmlFor="amount">
+          Montant
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            placeholder="20€..."
+            min="0"
+            max="10000000000"
+            required
+          />
+        </label>
+
+        <input type="hidden" id="user_id" name="user_id" value="1" />
+        <input type="hidden" id="category_id" name="category_id" value="1" />
+
+        <button type="submit">Enregistrer</button>
+      </Form>
     </>
   );
 }
