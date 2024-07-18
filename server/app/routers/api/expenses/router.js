00e2b6244800100b0473f60a7378690617774b39
Delgorithm <delgorithm@gmail.com>
2024-07-18 10:55:31 +0200

    Router : Added the delete

--- server/app/routers/api/expenses/router.js
+++ server/app/routers/api/expenses/router.js
@@ -2,10 +2,16 @@ const express = require("express");
 
 const router = express.Router();
 
-const { add, browse } = require("../../../controllers/expensesActions");
+const {
+  add,
+  browse,
+  destroy,
+} = require("../../../controllers/expensesActions");
 
 router.post("/", add);
 
 router.get("/", browse);
+
+router.delete("/:id", destroy);
 
 module.exports = router;
